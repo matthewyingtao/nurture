6e45b539ac72b90882c999cff756763daf8884c0
Matthew Tao <matthew.yingtao@gmail.com>
2022-02-21 19:03:04 +1300

    bump dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-/** @type {import('@sveltejs/kit').Config} */
 import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter({})
 	}
 };
 
